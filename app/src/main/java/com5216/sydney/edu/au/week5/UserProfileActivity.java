78b646ac487cdbad2f54d91bf456da26c4fd46d6
kaiyuan yang <3210048998@qq.com>
2022-09-19 16:15:58 +1000

    The first commit, compelte most of the front-end interface and the bottom tab still need to fix

---
+++ app/src/main/java/com5216/sydney/edu/au/week5/UserProfileActivity.java
@@ -0,0 +1,14 @@
+package com5216.sydney.edu.au.week5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
